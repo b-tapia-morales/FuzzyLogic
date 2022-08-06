f5b59048a87d3d2a7559f883c345353d3c33f97e
bairon-tapia <btapiam@alumnosuls.cl>
2022-08-06 16:57:48 -0400

    Generified the trapezoidal function

---
+++ FuzzyLogic/MembershipFunction/ITrapezoidalFunction.cs
@@ -0,0 +1,12 @@
+﻿namespace FuzzyLogic.MembershipFunction;
+
+public interface ITrapezoidalFunction<T> : IMembershipFunction<T>
+{
+    T LowerBoundary();
+
+    T UpperBoundary();
+
+    (T X0, T X1) CoreBoundaries();
+
+    ((T X0, T X1) Lower, (T X0, T X1) Upper) SupportBoundaries();
+}
