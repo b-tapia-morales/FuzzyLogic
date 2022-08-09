75ab0dcb03404ecc5b57b8720744f2844749c325
bairon-tapia <btapiam@alumnosuls.cl>
2022-08-09 01:31:47 -0400

    Added base class for fuzzy condition along with its operators

---
+++ FuzzyLogic/Clause/ConnectiveToken.cs
@@ -0,0 +1,9 @@
+﻿namespace FuzzyLogic.Clause;
+
+public enum ConnectiveToken
+{
+    Antecedent = 1,
+    Consequent = 2,
+    Conjunction = 3,
+    Disjunction = 4
+}
---
+++ FuzzyLogic/Clause/FuzzyCondition.cs
@@ -0,0 +1,11 @@
+﻿namespace FuzzyLogic.Clause;
+
+public class FuzzyCondition
+{
+    public ConnectiveToken ConnectiveToken { get; }
+
+    public FuzzyCondition(ConnectiveToken connectiveToken)
+    {
+        ConnectiveToken = connectiveToken;
+    }
+}
