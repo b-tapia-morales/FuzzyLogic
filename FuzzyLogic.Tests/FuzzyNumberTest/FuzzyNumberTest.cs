fe148f54beced120addd25d56c09c91a62d0adc8
bairon-tapia <btapiam@alumnosuls.cl>
2022-08-13 00:53:25 -0400

    Added testing for Fuzzy Number class

---
+++ FuzzyLogic.Tests/FuzzyNumberTest/FuzzyNumberTest.cs
@@ -0,0 +1,105 @@
+﻿using Xunit;
+using static FuzzyLogic.FuzzyNumber;
+
+namespace FuzzyLogic.Tests.FuzzyNumberTest;
+
+public class FuzzyNumberTest
+{
+    [Fact]
+    public void InstantiationThrowsExceptionOnValueOutOfRange()
+    {
+        Assert.Throws<ArgumentException>(() => Of(double.MinValue));
+        Assert.Throws<ArgumentException>(() => Of(-0.0001));
+        Assert.Throws<ArgumentException>(() => Of(+1.0001));
+        Assert.Throws<ArgumentException>(() => Of(double.MaxValue));
+    }
+
+    [Fact]
+    public void TryCreateFailsOnValueOutOfRange()
+    {
+        Assert.False(TryCreate(double.MinValue, out _));
+        Assert.False(TryCreate(-0.00001, out _));
+        Assert.False(TryCreate(+1.00001, out _));
+        Assert.False(TryCreate(double.MaxValue, out _));
+    }
+
+    [Fact]
+    public void MinMaxOperatorsYieldExpectedValues()
+    {
+        Assert.Equal(Of(0), MinValue & MaxValue, Tolerance);
+        Assert.Equal(Of(1), MinValue | MaxValue, Tolerance);
+    }
+
+    [Theory]
+    [MemberData(nameof(GenerateRandomPairs), parameters: 100)]
+    public void MinMaxOperatorsAreEquivalent(double x, double y)
+    {
+        Assert.Equal(Math.Min(x, y), Of(x) & Of(y), Tolerance);
+        Assert.Equal(Math.Max(x, y), Of(x) | Of(y), Tolerance);
+    }
+
+    [Fact]
+    public void NegationOperatorYieldsExpectedValues()
+    {
+        Assert.Equal(Of(0), !Of(1), Tolerance);
+        Assert.True(Of(0) == !Of(1));
+        Assert.Equal(Of(1), !Of(0), Tolerance);
+        Assert.True(Of(1) == !Of(0));
+    }
+
+    [Theory]
+    [MemberData(nameof(GenerateRandomValues), parameters: 100)]
+    public void NegationOperatorIsEquivalent(double x)
+    {
+        Assert.Equal(1 - x, !Of(x), Tolerance);
+        Assert.True(Of(1 - x) == !Of(x));
+    }
+
+    [Theory]
+    [InlineData(0.100001, 0.10000100)]
+    [InlineData(0.100001, 0.1000099999)]
+    [InlineData(-0.000001, 0)]
+    [InlineData(+0.000001, 0)]
+    [InlineData(0.999999, 1)]
+    [InlineData(1.000001, 1)]
+    public void EdgeCasesYieldEqualityForDifferenceUnderTolerance(double first, double second)
+    {
+        ValuesYieldEqualityForDifferenceUnderTolerance(first, second);
+    }
+
+    [Theory]
+    [MemberData(nameof(GenerateRandomValuesIncludingTolerance), parameters: 100)]
+    public void ValuesYieldEqualityForDifferenceUnderTolerance(double first, double second)
+    {
+        Assert.Equal(Of(first), Of(second), Tolerance);
+        Assert.True(Of(first) == Of(second));
+    }
+    
+    public static IEnumerable<object[]> GenerateRandomValues(int n)
+    {
+        var random = new Random();
+        for (var i = 1; i <= n; i++)
+        {
+            yield return new object[] {random.NextDouble()};
+        }
+    }
+    
+    public static IEnumerable<object[]> GenerateRandomPairs(int n)
+    {
+        var random = new Random();
+        for (var i = 1; i <= n; i++)
+        {
+            yield return new object[] {random.NextDouble(), random.NextDouble()};
+        }
+    }
+    
+    public static IEnumerable<object[]> GenerateRandomValuesIncludingTolerance(int n)
+    {
+        var random = new Random();
+        for (var i = 1; i <= n; i++)
+        {
+            var x = random.NextDouble();
+            yield return new object[] {x, x - 1e-6};
+        }
+    }
+}
