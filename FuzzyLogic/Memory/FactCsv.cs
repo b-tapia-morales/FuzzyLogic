706bb2f4bae54555111db63525af0f04f01940a1
bairon-tapia <btapiam@alumnosuls.cl>
2022-08-13 23:35:07 -0400

    Added primitive csv file and its correspondant class for facts retrieval

---
+++ FuzzyLogic/Memory/FactCsv.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration;
+
+namespace FuzzyLogic.Memory;
+
+public sealed class FactRow<T> where T : unmanaged, IConvertible
+{
+    public string Key { get; set; } = null!;
+    public T Value { get; set; } = default;
+
+    public override string ToString() => $"{Key} - {Value}";
+}
+
+public sealed class FactMapping<T> : ClassMap<FactRow<T>> where T : unmanaged, IConvertible
+{
+    public FactMapping()
+    {
+        Map(p => p.Key).Index(0);
+        Map(p => p.Value).Index(1);
+    }
+}
