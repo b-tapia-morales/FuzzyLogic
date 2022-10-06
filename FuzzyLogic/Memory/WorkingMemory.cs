d999ade9bfe414740b25e12bf24e059d6c2259e3
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-10-06 11:15:26 -0300

    Moved Entry Resolution Method into Working Memory class

--- FuzzyLogic/Memory/WorkingMemory.cs
+++ FuzzyLogic/Memory/WorkingMemory.cs
@@ -11,19 +11,19 @@ public class WorkingMemory : IWorkingMemory
     private static readonly string CsvFilePath =
         Path.Combine(Directory.GetCurrentDirectory(), CsvFileFolder, CsvFileName);
 
-    public WorkingMemory(EntryResolutionMethod method = Keep)
+    public WorkingMemory(EntryResolutionMethod method = Preserve)
     {
         Facts = new Dictionary<string, double>();
         Method = method;
     }
 
-    public WorkingMemory(Dictionary<string, double> dictionary, EntryResolutionMethod method = Keep)
+    public WorkingMemory(IDictionary<string, double> dictionary, EntryResolutionMethod method = Preserve)
     {
         Facts = dictionary;
         Method = method;
     }
 
-    public Dictionary<string, double> Facts { get; }
+    public IDictionary<string, double> Facts { get; }
     public EntryResolutionMethod Method { get; }
 
     public bool ContainsFact(string key) => Facts.ContainsKey(key);
@@ -38,12 +38,25 @@ public class WorkingMemory : IWorkingMemory
         }
     }
 
-    public override string ToString() => string.Join('\n', Facts);
+    public void UpdateFact(string key, double value)
+    {
+        if (!Facts.TryAdd(key, value))
+        {
+            Facts[key] = value;
+        }
+    }
 
-    public static WorkingMemory InitializeFromFile(string folderPath, EntryResolutionMethod method = Keep) =>
+    public bool RemoveFact(string key)
+    {
+        return Facts.Remove(key);
+    }
+
+    public override string ToString() => string.Join(Environment.NewLine, Facts.Select(e => $"{e.Key} = {e.Value}"));
+
+    public static WorkingMemory InitializeFromFile(string folderPath, EntryResolutionMethod method = Preserve) =>
         new(RowRetrieval.RetrieveRows<FactRow, FactMapping>(folderPath).ToDictionary(e => e.Key, e => e.Value),
             method);
 
-    public static WorkingMemory InitializeFromFile(EntryResolutionMethod method = Keep) =>
+    public static WorkingMemory InitializeFromFile(EntryResolutionMethod method = Preserve) =>
         InitializeFromFile(CsvFilePath, method);
 }
