b054c5ac082362b4b1dad44310156d56c674173c
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-10-16 14:45:38 -0300

    Added example of linguistic and rule bases creation

--- FuzzyLogic/Program.cs
+++ FuzzyLogic/Program.cs
@@ -1,29 +1,48 @@
-﻿using FuzzyLogic.Linguistics;
+﻿using FuzzyLogic.Knowledge;
+using FuzzyLogic.Linguistics;
 using FuzzyLogic.Memory;
+using FuzzyLogic.Proposition;
+using FuzzyLogic.Proposition.Enums;
 using FuzzyLogic.Rule;
+using MathNet.Numerics.Integration;
 
-var water = new LinguisticVariable("Water");
-water.AddTrapezoidFunction("Cold", 0, 0, 20, 40);
-water.AddTriangularFunction("Warm", 30, 50, 70);
-water.AddTrapezoidFunction("Hot", 50, 80, 100, 100);
-water.AddGaussianFunction("Sorta hot", 16, 4);
-Console.WriteLine(water.Is("Cold"));
-Console.WriteLine(water.IsNot("Sorta hot"));
-var random = new Random();
-foreach (var function in water.LinguisticValues)
-{
-    var randomInt = random.Next(12, 48);
-    Console.WriteLine(function.BoundaryInterval());
-    Console.WriteLine(function.ToPoint(randomInt));
-    Console.WriteLine($"Random int: {randomInt} - Membership degree: {function.MembershipDegree(randomInt)}");
-}
+var linguistics = LinguisticBase
+    .Create()
+    .AddLinguisticVariable(
+        LinguisticVariable
+            .Create("Water")
+            .AddTrapezoidFunction("Cold", 0, 0, 20, 40)
+            .AddTriangularFunction("Warm", 30, 50, 70)
+            .AddTrapezoidFunction("Hot", 50, 80, 100, 100)
+    ).AddLinguisticVariable(
+        LinguisticVariable
+            .Create("Power")
+            .AddTriangularFunction("Low", 0, 25, 50)
+            .AddTriangularFunction("High", 25, 50, 75)
+    );
 
-var rule = FuzzyRule
-    .Initialize()
-    .If(water.Is("Hot"))
-    .Or(water.Is("Sorta hot"))
-    .Then(water.IsNot("Cold"));
-Console.WriteLine(rule);
+var rules = RuleBase
+    .Create()
+    .AddRule(
+        FuzzyRule
+            .Create()
+            .If(FuzzyProposition.Is(linguistics, "Water", "Hot", HedgeToken.Slightly))
+            .And(FuzzyProposition.Is(linguistics, "Power", "High"))
+            .Then(FuzzyProposition.Is(linguistics, "Water", "Warm"))
+    ).AddRule(
+        FuzzyRule
+            .Create()
+            .If(FuzzyProposition.Is(linguistics, "Water", "Cold"))
+            .And(FuzzyProposition.Is(linguistics, "Power", "Low"))
+            .Then(FuzzyProposition.Is(linguistics, "Water", "Warm"))
+    );
+
+foreach (var rule in rules.ProductionRules)
+    Console.WriteLine(rule);
 
 var workingMemory = WorkingMemory.InitializeFromFile();
 Console.WriteLine(workingMemory);
+
+var function = linguistics.RetrieveLinguisticEntry("Water", "Cold")!.LambdaCutFunction(0.2);
+var value = NewtonCotesTrapeziumRule.IntegrateAdaptive(function, 0, 40, 1e-10);
+Console.WriteLine(value);
