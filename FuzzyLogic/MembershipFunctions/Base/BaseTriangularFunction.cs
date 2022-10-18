f45ea1898c9f2314ab229b91f54e7e480fab0c45
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-10-18 12:36:43 -0300

    Fixed evaluation bug in triangular function; now overriding base membership function method

--- FuzzyLogic/MembershipFunctions/Base/BaseTriangularFunction.cs
+++ FuzzyLogic/MembershipFunctions/Base/BaseTriangularFunction.cs
@@ -8,4 +8,12 @@ public abstract class BaseTriangularFunction<T> : BaseTrapezoidalFunction<T>
     protected BaseTriangularFunction(string name, T a, T b, T c) : base(name, a, b, b, c)
     {
     }
+    
+    public override Func<T, double> SimpleFunction() => x =>
+    {
+        if (x > A && x < B) return (x.ToDouble(null) - A.ToDouble(null)) / (B.ToDouble(null) - A.ToDouble(null));
+        if (x == B) return 1.0;
+        if (x > B && x < C) return (C.ToDouble(null) - x.ToDouble(null)) / (C.ToDouble(null) - B.ToDouble(null));
+        return 0.0;
+    };
 }
