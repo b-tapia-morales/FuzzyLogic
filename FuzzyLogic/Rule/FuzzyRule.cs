2f072a25613918c9c1663e01b37ba9e30b01bd94
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-10-18 12:38:49 -0300

    BIG milestone: Implemented methods to apply fuzzy implication

--- FuzzyLogic/Rule/FuzzyRule.cs
+++ FuzzyLogic/Rule/FuzzyRule.cs
@@ -1,4 +1,7 @@
-﻿using FuzzyLogic.Proposition;
+﻿using System.Collections.Immutable;
+using FuzzyLogic.MembershipFunctions.Real;
+using FuzzyLogic.Number;
+using FuzzyLogic.Proposition;
 using FuzzyLogic.Proposition.Enums;
 
 namespace FuzzyLogic.Rule;
@@ -20,6 +23,54 @@ public class FuzzyRule : IRule
     public IRule Or(IProposition proposition) => Or(this, proposition);
 
     public IRule Then(IProposition proposition) => Then(this, proposition);
+
+    public bool IsValid() => Antecedent != null && Consequent != null;
+
+    public bool IsApplicable(IDictionary<string, double> facts) =>
+        Antecedent != null && Consequent != null &&
+        facts.ContainsKey(Antecedent.LinguisticVariable.Name) &&
+        Connectives.All(e => facts.ContainsKey(e.LinguisticVariable.Name));
+
+    public IEnumerable<FuzzyNumber> ApplyOperators(IDictionary<string, double> facts)
+    {
+        if (!IsApplicable(facts))
+            return ImmutableList<FuzzyNumber>.Empty;
+
+        var propositions = Connectives.Prepend(Antecedent!);
+
+        return from proposition in propositions
+            let crispNumber = facts[proposition.LinguisticVariable.Name]
+            let membershipFunction = proposition.Function.SimpleFunction()
+            let hedgeFunction = proposition.LinguisticHedge.Function
+            let literalFunction = proposition.Literal.Function!
+            select literalFunction(hedgeFunction(membershipFunction(crispNumber)));
+    }
+
+    public FuzzyNumber? AggregateOperators(IDictionary<string, double> facts)
+    {
+        var numbers = new Queue<FuzzyNumber>(ApplyOperators(facts));
+        if (!numbers.Any())
+            return null;
+        if (numbers.Count == 1)
+            return numbers.First();
+
+        var connectives = new Queue<Connective>(Connectives.Select(e => e.Connective));
+        while (numbers.Count > 1)
+        {
+            var first = numbers.Dequeue();
+            var second = numbers.Dequeue();
+            var connectorFunction = connectives.Dequeue().Function!;
+            numbers.Enqueue(connectorFunction(first, second));
+        }
+
+        return numbers.Dequeue();
+    }
+
+    public Func<double, double>? ApplyImplication(IDictionary<string, double> facts)
+    {
+        var ruleWeight = AggregateOperators(facts);
+        return ruleWeight == null ? null : Consequent!.Function.LambdaCutFunction(ruleWeight);
+    }
 
     public static IRule Create() => new FuzzyRule();
 
@@ -58,6 +109,8 @@ public class FuzzyRule : IRule
 
     public static IRule Then(IRule rule, IProposition proposition)
     {
+        if (rule.IsFinalized) throw new FinalizedRuleException();
+
         if (rule.Antecedent == null) throw new MissingAntecedentException();
 
         proposition.Connective = Connective.Then;
