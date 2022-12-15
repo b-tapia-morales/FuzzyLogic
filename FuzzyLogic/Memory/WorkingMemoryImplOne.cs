65dd839cfc94a73a70aa49ebc6bc7b93af494bd9
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-12-15 20:22:48 -0300

    Added example for first Working Memory Implementation

---
+++ FuzzyLogic/Memory/WorkingMemoryImplOne.cs
@@ -0,0 +1,15 @@
+﻿using static FuzzyLogic.Memory.EntryResolutionMethod;
+
+namespace FuzzyLogic.Memory;
+
+public class WorkingMemoryImplOne : WorkingMemory
+{
+    public new static IWorkingMemory Initialize(EntryResolutionMethod method = Replace)
+    {
+        var workingMemory = Create();
+        workingMemory.AddFact("Horario", 12);
+        workingMemory.AddFact("Área", 150);
+        workingMemory.AddFact("Espesor", 0.2);
+        return workingMemory;
+    }
+}
