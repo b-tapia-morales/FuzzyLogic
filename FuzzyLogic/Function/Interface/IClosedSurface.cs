790e00fc4e243c9e72c08321ed026fa7dfad9f24
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-12-16 14:03:44 -0300

    Renamed interface to better represent its funcionality

---
+++ FuzzyLogic/Function/Interface/IClosedSurface.cs
@@ -0,0 +1,22 @@
+﻿using FuzzyLogic.Number;
+using MathNet.Numerics.Integration;
+
+namespace FuzzyLogic.Function.Interface;
+
+public interface IClosedSurface
+{
+    const double DefaultErrorMargin = 1e-5;
+
+    double CalculateArea(double errorMargin = DefaultErrorMargin);
+
+    double CalculateArea(FuzzyNumber y, double errorMargin = DefaultErrorMargin);
+    
+    static double Integrate(Func<double, double> function, double x0, double x1,
+        double errorMargin = DefaultErrorMargin) =>
+        NewtonCotesTrapeziumRule.IntegrateAdaptive(function, x0, x1, errorMargin);
+    
+    static double DefaultCalculateArea(double errorMargin = DefaultErrorMargin)
+    {
+        return 0.0;
+    }
+}
