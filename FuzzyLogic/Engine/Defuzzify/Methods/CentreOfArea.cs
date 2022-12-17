24cd1c26f3c2f2df42f236c8a8303e22c0e293ae
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-12-16 23:40:51 -0300

    Implemented Area calculation method

--- FuzzyLogic/Engine/Defuzzify/Methods/CentreOfArea.cs
+++ FuzzyLogic/Engine/Defuzzify/Methods/CentreOfArea.cs
@@ -1,9 +1,16 @@
-﻿using FuzzyLogic.Rule;
+﻿using FuzzyLogic.Function.Interface;
+using FuzzyLogic.Rule;
 
 namespace FuzzyLogic.Engine.Defuzzify.Methods;
 
 public class CentreOfArea : IDefuzzifier
 {
-    public double? Defuzzify(ICollection<IRule> rules, IDictionary<string, double> facts) =>
-        throw new NotImplementedException();
+    public double? Defuzzify(ICollection<IRule> rules, IDictionary<string, double> facts)
+    {
+        var tuple = rules
+            .Select(e => (Function: e.Consequent!.Function, Area: e.CalculateArea(facts).GetValueOrDefault()))
+            .MaxBy(e => e.Area);
+        var function = (IClosedSurface) tuple.Function;
+        return function.CalculateCentroid().X0;
+    }
 }
