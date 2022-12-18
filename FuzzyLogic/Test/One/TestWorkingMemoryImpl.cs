ced4d02e55e9a1418d7cba4815ca48ad59ee2fef
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-12-17 22:42:26 -0300

    Added additional Working Memory for facts

---
+++ FuzzyLogic/Test/One/TestWorkingMemoryImpl.cs
@@ -0,0 +1,25 @@
+﻿using FuzzyLogic.Memory;
+
+namespace FuzzyLogic.Test.One;
+
+public class TestWorkingMemoryImpl: WorkingMemory
+{
+    private static readonly Random Random = new(Environment.TickCount);
+    
+    public new static IWorkingMemory Initialize()
+    {
+        var workingMemory = Create();
+        workingMemory.AddFact("Ret", Random.NextDouble());
+        workingMemory.AddFact("Reg", Random.NextDouble());
+        workingMemory.AddFact("Cnt", Random.NextDouble());
+        workingMemory.AddFact("Dpj", Random.NextDouble());
+        workingMemory.AddFact("Pc", Random.NextDouble());
+        workingMemory.AddFact("Fz", Random.NextDouble());
+        workingMemory.AddFact("Rea", Random.NextDouble());
+        workingMemory.AddFact("Vlsp", Random.NextDouble());
+        workingMemory.AddFact("Elmp", Random.NextDouble());
+        workingMemory.AddFact("Vs", Random.NextDouble());
+        workingMemory.AddFact("Ag", Random.NextDouble());
+        return workingMemory;
+    }
+}
