5493856ad3d671336156d1962f84b950b2c8a9b2
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-12-18 19:14:27 -0300

    Fixed minor bug in ToString() method for rule

--- FuzzyLogic/Program.cs
+++ FuzzyLogic/Program.cs
@@ -4,13 +4,14 @@ using FuzzyLogic.Test.One;
 using FuzzyLogic.Tree;
 using static System.Globalization.CultureInfo;
 using static FuzzyLogic.Engine.Defuzzify.DefuzzificationMethod;
+using static FuzzyLogic.Memory.EntryResolutionMethod;
 using static FuzzyLogic.Rule.ComparingMethod;
 
 var linguisticBase = TestLinguisticImpl.Initialize();
 
 var ruleBase = TestRuleImpl.Initialize(linguisticBase, ShortestPremise);
 
-var workingMemory = TestWorkingMemoryImpl.Initialize();
+var workingMemory = TestWorkingMemoryImpl.Initialize(Replace);
 
 var knowledgeBase = KnowledgeBase.Create(linguisticBase, ruleBase);
 
