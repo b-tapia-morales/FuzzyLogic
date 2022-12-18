3cc16493404160d73d72d7cd544f5045a92d5738
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-12-18 13:22:23 -0300

    Created Comparer classes for rule conflict resolution

---
+++ FuzzyLogic/Rule/RuleComparer.cs
@@ -0,0 +1,47 @@
+﻿namespace FuzzyLogic.Rule;
+
+public class PriorityComparer : IComparer<IRule>
+{
+    public int Compare(IRule? x, IRule? y)
+    {
+        if (x == null && y == null) return +0;
+        if (x == null) return -1;
+        if (y == null) return +1;
+        var a = (int) x.Priority;
+        var b = (int) y.Priority;
+        return a.CompareTo(b);
+    }
+}
+
+public class ShortestPremiseComparer : IComparer<IRule>
+{
+    public int Compare(IRule? x, IRule? y)
+    {
+        if (x == null && y == null) return +0;
+        if (x == null) return -1;
+        if (y == null) return +1;
+        var a = 1 + x.Connectives.Count;
+        var b = 1 + y.Connectives.Count;
+        return -(a.CompareTo(b));
+    }
+}
+
+public class LargestPremiseComparer : IComparer<IRule>
+{
+    public int Compare(IRule? x, IRule? y)
+    {
+        if (x == null && y == null) return +0;
+        if (x == null) return -1;
+        if (y == null) return +1;
+        var a = 1 + x.Connectives.Count;
+        var b = 1 + y.Connectives.Count;
+        return a.CompareTo(b);
+    }
+}
+
+public enum Comparer
+{
+    Priority = 1,
+    ShortestPremise = 2,
+    LargestPremise = 3
+}
