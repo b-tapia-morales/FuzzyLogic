12ea8369347bd152b6b0cd344d79835f1a88f1e2
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-12-18 21:49:43 -0300

    Minor tweaks

--- FuzzyLogic/Program.cs
+++ FuzzyLogic/Program.cs
@@ -1,6 +1,7 @@
 ﻿using FuzzyLogic.Engine;
 using FuzzyLogic.Knowledge;
 using FuzzyLogic.Test.One;
+using FuzzyLogic.Test.Two;
 using static System.Globalization.CultureInfo;
 using static FuzzyLogic.Engine.Defuzzify.DefuzzificationMethod;
 using static FuzzyLogic.Memory.EntryResolutionMethod;
@@ -8,14 +9,15 @@ using static FuzzyLogic.Rule.ComparingMethod;
 
 var linguisticBase = TestLinguisticImpl.Initialize();
 
-var ruleBase = TestRuleImpl.Initialize(linguisticBase, ShortestPremise);
+var ruleBase = TestRuleImpl.Initialize(linguisticBase);
 
-var workingMemory = TestWorkingMemoryImpl.Initialize(Replace);
+var workingMemory = TestWorkingMemoryImpl.Initialize();
 
 var knowledgeBase = KnowledgeBase.Create(linguisticBase, ruleBase);
 
-var inferenceEngine = InferenceEngine.Create(knowledgeBase, workingMemory, CentreOfArea);
+var inferenceEngine = InferenceEngine.Create(knowledgeBase, workingMemory, LastOfMaxima);
 
 var value = inferenceEngine.Defuzzify("Hab");
 Console.WriteLine();
-Console.WriteLine($"Has the fact been successfully inferred? {value?.ToString(InvariantCulture)}");
+Console.WriteLine("Has the fact been successfully inferred?");
+Console.WriteLine($"{(value == null ? "NO" : value.Value.ToString(InvariantCulture))}");
