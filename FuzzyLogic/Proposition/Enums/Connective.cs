a055835f861de195dfa5276afcc95bcf5bbaa21d
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2024-12-09 18:02:55 -0300

    Removed default operators for Connective and Literal classes

--- FuzzyLogic/Proposition/Enums/Connective.cs
+++ FuzzyLogic/Proposition/Enums/Connective.cs
@@ -1,40 +1,30 @@
 ﻿using Ardalis.SmartEnum;
 using FuzzyLogic.Enum;
-using FuzzyLogic.Number;
 
 namespace FuzzyLogic.Proposition.Enums;
 
 public class Connective : SmartEnum<Connective>, IEnum<Connective, ConnectiveToken>
 {
-    public static readonly Connective None =
-        new(nameof(None), string.Empty, null, (int) ConnectiveToken.None);
-
     public static readonly Connective If =
-        new(nameof(If), "IF", null, (int) ConnectiveToken.Antecedent);
+        new(nameof(If), "IF", (int) ConnectiveToken.Antecedent);
 
     public static readonly Connective And =
-        new(nameof(And), "AND", (a, b) => a & b, (int) ConnectiveToken.Conjunction);
+        new(nameof(And), "AND", (int) ConnectiveToken.Conjunction);
 
     public static readonly Connective Or =
-        new(nameof(Or), "OR", (a, b) => a | b, (int) ConnectiveToken.Disjunction);
+        new(nameof(Or), "OR", (int) ConnectiveToken.Disjunction);
 
     public static readonly Connective Then =
-        new(nameof(Then), "THEN", (a, b) => a >> b, (int) ConnectiveToken.Consequent);
+        new(nameof(Then), "THEN", (int) ConnectiveToken.Consequent);
 
-    private Connective(string name, string readableName, Func<FuzzyNumber, FuzzyNumber, FuzzyNumber>? function,
-        int value) : base(name, value)
-    {
+    private Connective(string name, string readableName, int value) : base(name, value) =>
         ReadableName = readableName;
-        Function = function;
-    }
 
     public string ReadableName { get; }
-    public Func<FuzzyNumber, FuzzyNumber, FuzzyNumber>? Function { get; }
 }
 
 public enum ConnectiveToken
 {
-    None = 0,
     Antecedent = 1,
     Consequent = 2,
     Conjunction = 3,
--- FuzzyLogic/Proposition/Enums/Literal.cs
+++ FuzzyLogic/Proposition/Enums/Literal.cs
@@ -1,34 +1,24 @@
 ﻿using Ardalis.SmartEnum;
 using FuzzyLogic.Enum;
-using FuzzyLogic.Number;
 
 namespace FuzzyLogic.Proposition.Enums;
 
 public class Literal : SmartEnum<Literal>, IEnum<Literal, LiteralToken>
 {
-    public static readonly Literal None =
-        new(nameof(None), string.Empty, null, (int) LiteralToken.None);
-
     public static readonly Literal Is =
-        new(nameof(Is), "IS", y => y, (int) LiteralToken.Affirmation);
+        new(nameof(Is), "IS", (int) LiteralToken.Affirmation);
 
     public static readonly Literal IsNot =
-        new(nameof(IsNot), "IS NOT", y => !y, (int) LiteralToken.Negation);
+        new(nameof(IsNot), "IS NOT", (int) LiteralToken.Negation);
 
-    private Literal(string name, string readableName, Func<FuzzyNumber, FuzzyNumber>? function,
-        int value) : base(name, value)
-    {
+    private Literal(string name, string readableName, int value) : base(name, value) =>
         ReadableName = readableName;
-        Function = function;
-    }
 
     public string ReadableName { get; }
-    public Func<FuzzyNumber, FuzzyNumber>? Function { get; }
 }
 
 public enum LiteralToken
 {
-    None = 0,
-    Affirmation = 1,
-    Negation = 2
+    Affirmation,
+    Negation
 }
