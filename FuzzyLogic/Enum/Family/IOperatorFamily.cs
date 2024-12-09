b83cc23b9266dffc19403c9631a6cb0041b41a1f
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2024-12-09 17:47:10 -0300

    Now using operator families to allow for creation of specific operator sets

---
+++ FuzzyLogic/Enum/Family/IOperatorFamily.cs
@@ -0,0 +1,23 @@
+﻿using FuzzyLogic.Enum.Negation;
+using FuzzyLogic.Enum.Residuum;
+using FuzzyLogic.Enum.TConorm;
+using FuzzyLogic.Enum.TNorm;
+using FuzzyLogic.Number;
+
+namespace FuzzyLogic.Enum.Family;
+
+public interface IOperatorFamily : INegation, INorm, IConorm, IResiduum
+{
+    INegation INegation { get; set; }
+    INorm Norm { get; set; }
+    IConorm Conorm { get; set; }
+    IResiduum Residuum { get; set; }
+
+    FuzzyNumber INegation.Complement(FuzzyNumber x) => INegation.Complement(x);
+
+    FuzzyNumber INorm.Intersection(FuzzyNumber x, FuzzyNumber y) => Norm.Intersection(x, y);
+
+    FuzzyNumber IConorm.Union(FuzzyNumber x, FuzzyNumber y) => Conorm.Union(x, y);
+
+    FuzzyNumber IResiduum.Implication(FuzzyNumber x, FuzzyNumber y) => Residuum.Implication(x, y);
+}
---
+++ FuzzyLogic/Enum/Family/OperatorFamily.cs
@@ -0,0 +1,28 @@
+﻿using FuzzyLogic.Enum.Negation;
+using FuzzyLogic.Enum.Residuum;
+using FuzzyLogic.Enum.TConorm;
+using FuzzyLogic.Enum.TNorm;
+
+namespace FuzzyLogic.Enum.Family;
+
+public class OperatorFamily(INegation negation, INorm norm, IConorm conorm, IResiduum residuum) : IOperatorFamily
+{
+    public INegation INegation { get; set; } = negation;
+    public INorm Norm { get; set; } = norm;
+    public IConorm Conorm { get; set; } = conorm;
+    public IResiduum Residuum { get; set; } = residuum;
+}
+
+public enum CanonicalType
+{
+    Godel,
+    Product,
+    Lukasiewicz,
+    Nilpotent
+}
+
+public enum ParameterizedType
+{
+    Hamacher,
+    Sugeno
+}
