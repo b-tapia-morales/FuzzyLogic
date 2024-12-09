2470a0afaea1c8ab99fc2bb0bfd4e4413af089e9
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2024-12-09 17:45:35 -0300

    Now using enums for t-norm, t-conorm, negator and implication operators instead of class based implementations.

--- FuzzyLogic/Enum/IEnum.cs
+++ FuzzyLogic/Enum/IEnum.cs
@@ -7,33 +7,40 @@ public interface IEnum<out T, TEnum>
     where T : SmartEnum<T>, IEnum<T, TEnum>
     where TEnum : struct, System.Enum, IConvertible
 {
-    private static readonly ImmutableList<TEnum> Tokens =
-        System.Enum.GetValues<TEnum>().OrderBy(e => e.ToInt32(null)).ToImmutableList();
-
-    static IReadOnlyList<T> Values { get; } = SmartEnum<T>.List.OrderBy(e => e.Value).ToImmutableList();
-
-    static IReadOnlyList<T> NonNullValues { get; } = Values.Skip(1).ToImmutableList();
-
-    static IReadOnlyDictionary<TEnum, T> TokenDictionary { get; } =
-        Tokens.Zip(Values, (k, v) => new { Key = k, Value = v }).ToImmutableSortedDictionary(e => e.Key, e => e.Value);
-
-    static IReadOnlyDictionary<T, TEnum> ReverseTokenDictionary { get; } =
-        Tokens.Zip(Values, (k, v) => new { Key = k, Value = v }).ToImmutableSortedDictionary(e => e.Value, e => e.Key);
-
     string ReadableName { get; }
 
-    static T ToValue(TEnum token) =>
-        TokenDictionary.TryGetValue(token, out var value) ? value : throw new KeyNotFoundException();
+    static T ToValue(TEnum @enum) =>
+        EnumDict.TryGetValue(@enum, out var value) ? value : throw new KeyNotFoundException();
 
-    static bool TryGetValue(TEnum token, out T? value) =>
-        TokenDictionary.TryGetValue(token, out value);
+    static T ToValue(string readableName) =>
+        ReadableNameDict.TryGetValue(readableName, out var value) ? value : throw new KeyNotFoundException();
 
-    static TEnum ToToken(T value) =>
-        ReverseTokenDictionary.TryGetValue(value, out var token) ? token : throw new KeyNotFoundException();
+    static bool TryGetValue(TEnum @enum, out T? value) =>
+        EnumDict.TryGetValue(@enum, out value);
 
-    static bool TryGetToken(T value, out TEnum? token)
+    static bool TryGetValue(string readableName, out T? value) =>
+        ReadableNameDict.TryGetValue(readableName, out value);
+
+    static TEnum ToEnum(T value) =>
+        ValueDict.TryGetValue(value, out var token) ? token : throw new KeyNotFoundException();
+
+    static bool TryGetEnum(T value, out TEnum? token)
     {
-        token = ReverseTokenDictionary.TryGetValue(value, out var enumValue) ? enumValue : null;
+        token = ValueDict.TryGetValue(value, out var enumValue) ? enumValue : null;
         return token != null;
     }
+
+    private static readonly ImmutableList<TEnum> Enums =
+        System.Enum.GetValues<TEnum>().OrderBy(e => e.ToInt32(null)).ToImmutableList();
+
+    private static IReadOnlyList<T> Values { get; } = SmartEnum<T>.List.OrderBy(e => e.Value).ToImmutableList();
+
+    private static IReadOnlyDictionary<TEnum, T> EnumDict { get; } =
+        Enums.Zip(Values, (k, v) => new {Key = k, Value = v}).ToImmutableSortedDictionary(e => e.Key, e => e.Value);
+
+    private static IReadOnlyDictionary<T, TEnum> ValueDict { get; } =
+        Enums.Zip(Values, (k, v) => new {Key = k, Value = v}).ToImmutableSortedDictionary(e => e.Value, e => e.Key);
+
+    private static IReadOnlyDictionary<string, T> ReadableNameDict { get; } =
+        Values.ToImmutableSortedDictionary(e => e.ReadableName, e => e, StringComparer.InvariantCultureIgnoreCase);
 }
