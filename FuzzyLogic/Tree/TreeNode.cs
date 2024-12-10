95a236f0d7c5fed7858b04b3e7053da214d17f2d
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2024-12-10 00:59:43 -0300

    Moved the detection of circular dependencies to its own class

--- FuzzyLogic/Tree/TreeNode.cs
+++ FuzzyLogic/Tree/TreeNode.cs
@@ -1,92 +1,55 @@
 ﻿using FuzzyLogic.Engine.Defuzzify;
+using FuzzyLogic.Enum.Family;
 using FuzzyLogic.Knowledge.Rule;
-using FuzzyLogic.Number;
 using FuzzyLogic.Rule;
 
 namespace FuzzyLogic.Tree;
 
-public class TreeNode<T> where T : struct, IFuzzyNumber<T>
+public class TreeNode
 {
     public string VariableName { get; }
-    public ICollection<IRule<T>> Rules { get; } = new List<IRule<T>>();
-    public ICollection<TreeNode<T>> Children { get; } = new List<TreeNode<T>>();
-    public bool IsProven { get; set; }
+    public ICollection<IRule> Rules { get; } = new List<IRule>();
+    public ICollection<TreeNode> Children { get; } = new List<TreeNode>();
+    public bool IsProven { get; private set; }
 
-    public TreeNode(string variableName) => VariableName = variableName;
+    private TreeNode(string variableName) => VariableName = variableName;
 
-    public bool IsLeaf() => !Children.Any();
+    public bool IsLeaf() => Children.Count == 0;
 
-    public void AddRules(IEnumerable<IRule<T>> rules)
+    private void AddRules(IEnumerable<IRule> rules)
     {
         foreach (var rule in rules)
             Rules.Add(rule);
     }
 
-    public void AddChild(TreeNode<T> child) => Children.Add(child);
-
-    public void AddChildren(IEnumerable<TreeNode<T>> children)
+    private void AddChildren(IEnumerable<TreeNode> children)
     {
         foreach (var child in children)
             Children.Add(child);
     }
 
-    public void WriteNode()
+    public double? InferFact(IDictionary<string, double> facts, IDefuzzifier defuzzifier, IOperatorFamily operatorFamily,
+        ImplicationMethod method = ImplicationMethod.Mamdani) =>
+        InferFact(this, facts, defuzzifier, operatorFamily, method);
+
+    public static TreeNode CreateDerivationTree(string variableName, ICollection<IRule> rules,
+        IComparer<IRule> ruleComparer, IDictionary<string, double> facts)
     {
-        Console.WriteLine($"Linguistic variable name: {VariableName}");
-        if (!IsLeaf())
-            Console.WriteLine($"Associated rules:{Environment.NewLine}{string.Join(Environment.NewLine, Rules)}");
-        Console.WriteLine($"Is it proven? {IsProven}");
-    }
-
-    public void WriteTree() => WriteTree(this);
-
-    public void PrettyWriteTree() => PrettyWriteTree(this);
-
-    public double? InferFact(IDictionary<string, double> facts, IDefuzzifier<T> defuzzifier) =>
-        InferFact(this, facts, defuzzifier);
-
-    public static TreeNode<T> CreateDerivationTree(string variableName, ICollection<IRule<T>> rules,
-        IComparer<IRule<T>> ruleComparer, IDictionary<string, double> facts)
-    {
-        var rootNode = new TreeNode<T>(variableName);
-        var stack = new Stack<TreeNode<T>>();
+        var rootNode = new TreeNode(variableName);
+        var stack = new Stack<TreeNode>();
         stack.Push(rootNode);
-        var circularDependencies = new LinkedList<string>();
         while (stack.TryPop(out var node))
         {
-            if (circularDependencies.Any())
-                circularDependencies.RemoveFirst();
-            UpdateNode(node, node.VariableName, rules, ruleComparer, facts, circularDependencies);
+            UpdateNode(node, node.VariableName, rules, ruleComparer, facts);
             foreach (var child in node.Children)
                 stack.Push(child);
-            circularDependencies.AddFirst(node.VariableName);
         }
 
         return rootNode;
     }
 
-    private static void WriteTree(TreeNode<T> rootNode)
-    {
-        var stack = new Stack<TreeNode<T>>();
-        stack.Push(rootNode);
-        while (stack.TryPop(out var node))
-        {
-            node.WriteNode();
-            foreach (var child in node.Children)
-                stack.Push(child);
-        }
-    }
-
-    private static void PrettyWriteTree(TreeNode<T> node, string indent = "", bool last = true)
-    {
-        Console.WriteLine(indent + "+- " + node.VariableName);
-        indent += last ? "   " : "|  ";
-        for (var i = 0; i < node.Children.Count; i++)
-            PrettyWriteTree(node.Children.ElementAt(i), indent, i == node.Children.Count - 1);
-    }
-
-    private static double? InferFact(TreeNode<T> rootNode, IDictionary<string, double> facts,
-        IDefuzzifier<T> defuzzifier)
+    private static double? InferFact(TreeNode rootNode, IDictionary<string, double> facts,
+        IDefuzzifier defuzzifier, IOperatorFamily operatorFamily, ImplicationMethod method = ImplicationMethod.Mamdani)
     {
         var stack = TraverseReverseLevelOrder(rootNode);
         while (stack.TryPop(out var node))
@@ -98,13 +61,13 @@ public class TreeNode<T> where T : struct, IFuzzyNumber<T>
             }
 
             var applicableRules = node.Rules.Where(e => e.IsApplicable(facts)).ToList();
-            if (!applicableRules.Any())
+            if (applicableRules.Count == 0)
             {
                 node.IsProven = false;
                 continue;
             }
 
-            var crispValue = defuzzifier.Defuzzify(applicableRules, facts);
+            var crispValue = defuzzifier.Defuzzify(applicableRules, facts, operatorFamily, method);
             if (crispValue != null)
                 facts.TryAdd(node.VariableName, crispValue.Value);
             node.IsProven = true;
@@ -115,36 +78,27 @@ public class TreeNode<T> where T : struct, IFuzzyNumber<T>
         return fact;
     }
 
-    private static void UpdateNode(TreeNode<T> node, string variableName, ICollection<IRule<T>> rules,
-        IComparer<IRule<T>> ruleComparer, IDictionary<string, double> facts, ICollection<string> circularDependencies)
+    private static void UpdateNode(TreeNode node, string variableName, ICollection<IRule> rules,
+        IComparer<IRule> ruleComparer, IDictionary<string, double> facts)
     {
         if (facts.ContainsKey(variableName))
             return;
-        var filteredRules = RuleBase<T>.FilterByResolutionMethod(variableName, rules, ruleComparer);
-        if (!filteredRules.Any())
+        var filteredRules = RuleBase.FilterByResolutionMethod(rules, variableName, ruleComparer);
+        if (filteredRules.Count == 0)
             return;
-        var antecedents = filteredRules
-            .Select(e => e.Antecedent!.LinguisticVariable.Name)
-            .Where(e => !circularDependencies.Contains(e))
-            .ToList();
-        var connectives = filteredRules
-            .SelectMany(e => e.Connectives)
-            .Select(e => e.LinguisticVariable.Name)
-            .Where(e => !circularDependencies.Contains(e))
-            .ToList();
-        var set = new HashSet<string>();
-        set.UnionWith(antecedents);
-        set.UnionWith(connectives);
-        var children = new List<TreeNode<T>>(set.Select(e => new TreeNode<T>(e)));
+        var antecedents = filteredRules.Select(e => e.Conditional!.VariableName).ToList();
+        var connectives = filteredRules.SelectMany(e => e.Connectives).Select(e => e.VariableName).ToList();
+        var set = new HashSet<string>(antecedents.Union(connectives));
+        var children = new List<TreeNode>(set.Select(e => new TreeNode(e)));
         node.AddRules(filteredRules);
         node.AddChildren(children);
     }
 
-    private static Stack<TreeNode<T>> TraverseReverseLevelOrder(TreeNode<T> rootNode)
+    private static Stack<TreeNode> TraverseReverseLevelOrder(TreeNode rootNode)
     {
-        var queue = new Queue<TreeNode<T>>();
+        var queue = new Queue<TreeNode>();
         queue.Enqueue(rootNode);
-        var stack = new Stack<TreeNode<T>>();
+        var stack = new Stack<TreeNode>();
         while (queue.TryDequeue(out var node))
         {
             stack.Push(node);
@@ -153,5 +107,51 @@ public class TreeNode<T> where T : struct, IFuzzyNumber<T>
         }
 
         return stack;
+    }
+}
+
+public static class TreeExtensions
+{
+    public static Stack<TreeNode> TraverseReverseLevelOrder(this TreeNode rootNode)
+    {
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(rootNode);
+        var stack = new Stack<TreeNode>();
+        while (queue.TryDequeue(out var node))
+        {
+            stack.Push(node);
+            foreach (var child in node.Children)
+                queue.Enqueue(child);
+        }
+
+        return stack;
+    }
+
+    public static void PrettyWriteTree(this TreeNode node, string indent = "", bool last = true)
+    {
+        Console.WriteLine($"{indent}+- {node.VariableName}");
+        indent += last ? "   " : "|  ";
+        for (var i = 0; i < node.Children.Count; i++)
+            PrettyWriteTree(node.Children.ElementAt(i), indent, i == node.Children.Count - 1);
+    }
+
+    public static void WriteTree(this TreeNode rootNode)
+    {
+        var stack = new Stack<TreeNode>();
+        stack.Push(rootNode);
+        while (stack.TryPop(out var node))
+        {
+            node.WriteNode();
+            foreach (var child in node.Children)
+                stack.Push(child);
+        }
+    }
+
+    private static void WriteNode(this TreeNode node)
+    {
+        Console.WriteLine($"Linguistic variable name: {node.VariableName}");
+        if (!node.IsLeaf())
+            Console.WriteLine($"Associated rules:{Environment.NewLine}{string.Join(Environment.NewLine, node.Rules)}");
+        Console.WriteLine($"Is it proven? {node.IsProven}");
     }
 }
