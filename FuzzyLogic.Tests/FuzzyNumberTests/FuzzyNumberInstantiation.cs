6a7796f292fcc6cabb4bf0f10b882de6a88ef280
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2024-12-11 01:35:54 -0300

    First working version for testing of the associativity law

---
+++ FuzzyLogic.Tests/FuzzyNumberTests/FuzzyNumberInstantiation.cs
@@ -0,0 +1,86 @@
+﻿using Xunit;
+
+namespace FuzzyLogic.Tests.FuzzyNumberTests;
+
+public class FuzzyNumberInstantiation
+{
+    private static readonly Random Random = new();
+
+    [Fact]
+    public void InstantiationThrowsExceptionOnValueOutOfRangeHelper()
+    {
+        Assert.Throws<ArgumentException>(() => Number.FuzzyNumber.Of(Number.FuzzyNumber.Epsilon * -1e1));
+        Assert.Throws<ArgumentException>(() => Number.FuzzyNumber.Of(1 + Number.FuzzyNumber.Epsilon * 1e1));
+        Assert.Throws<ArgumentException>(() => Number.FuzzyNumber.Of(double.MaxValue));
+    }
+
+    [Fact]
+    public void TryCreateFailsOnValueOutOfRangeHelper()
+    {
+        Assert.False(Number.FuzzyNumber.TryCreate(Number.FuzzyNumber.Epsilon * -1e1, out _));
+        Assert.False(Number.FuzzyNumber.TryCreate(1 + Number.FuzzyNumber.Epsilon * 1e1, out _));
+        Assert.False(Number.FuzzyNumber.TryCreate(double.MaxValue, out _));
+    }
+
+    [Fact]
+    public void NegationOperatorYieldsExpectedValuesHelper()
+    {
+        Assert.Equal(Number.FuzzyNumber.Min, !Number.FuzzyNumber.Max, Number.FuzzyNumber.Epsilon);
+        Assert.StrictEqual(Number.FuzzyNumber.Min, !Number.FuzzyNumber.Max);
+        Assert.True(Number.FuzzyNumber.Min == !Number.FuzzyNumber.Max);
+        Assert.Equal(Number.FuzzyNumber.Max, !Number.FuzzyNumber.Min, Number.FuzzyNumber.Epsilon);
+        Assert.StrictEqual(Number.FuzzyNumber.Max, !Number.FuzzyNumber.Min);
+        Assert.True(Number.FuzzyNumber.Max == !Number.FuzzyNumber.Min);
+    }
+
+    [Theory]
+    [MemberData(nameof(GenerateRandomValues), parameters: 100)]
+    public void NegationOperatorIsEquivalent(double x) =>
+        NegationOperatorIsEquivalentHelper(x);
+
+    [Theory]
+    [MemberData(nameof(GenerateRandomValuesIncludingTolerance), parameters: 100)]
+    public void ValuesYieldEqualityForDifferenceUnderTolerance(double first, double second) =>
+        ValuesYieldEqualityForDifferenceUnderToleranceHelper(first, second);
+
+    [Theory]
+    [InlineData(1e-1 + Number.FuzzyNumber.Epsilon, 1e-1 + Number.FuzzyNumber.Epsilon + 1e-15)]
+    [InlineData(0 - Number.FuzzyNumber.Epsilon * 1e-1, 0)]
+    [InlineData(0 + Number.FuzzyNumber.Epsilon * 1e-1, 0)]
+    [InlineData(1 - Number.FuzzyNumber.Epsilon * 1e-1, 1)]
+    [InlineData(1 + Number.FuzzyNumber.Epsilon * 1e-1, 1)]
+    public void EdgeCasesYieldEqualityForDifferenceUnderTolerance(double first, double second) =>
+        ValuesYieldEqualityForDifferenceUnderTolerance(first, second);
+
+    private static void NegationOperatorIsEquivalentHelper(double x)
+    {
+        Assert.Equal(1 - x, !Number.FuzzyNumber.Of(x), Number.FuzzyNumber.Epsilon);
+        Assert.True(Number.FuzzyNumber.Of(1 - x) == !Number.FuzzyNumber.Of(x));
+    }
+
+    private static void ValuesYieldEqualityForDifferenceUnderToleranceHelper(double a, double b)
+    {
+        Assert.Equal(Number.FuzzyNumber.Of(a), Number.FuzzyNumber.Of(b), Number.FuzzyNumber.Epsilon);
+        Assert.True(Number.FuzzyNumber.Of(a) == Number.FuzzyNumber.Of(b));
+    }
+
+    public static TheoryData<double> GenerateRandomValues(int n)
+    {
+        var data = new TheoryData<double>();
+        for (var i = 1; i <= n; i++)
+        {
+            data.Add(Random.NextDouble());
+        }
+
+        return data;
+    }
+
+    public static IEnumerable<object[]> GenerateRandomValuesIncludingTolerance(int n)
+    {
+        for (var i = 1; i <= n; i++)
+        {
+            var x = Random.NextDouble();
+            yield return [x, x - Random.Next(1, 9) * Number.FuzzyNumber.Epsilon * 1e-1];
+        }
+    }
+}
---
+++ FuzzyLogic.Tests/Operator/OperatorProperties.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using FuzzyLogic.Enum;
+using FuzzyLogic.Enum.Negation;
+using FuzzyLogic.Enum.TConorm;
+using FuzzyLogic.Enum.TNorm;
+using FuzzyLogic.Number;
+using Xunit;
+
+namespace FuzzyLogic.Tests.Operator;
+
+public class OperatorProperties
+{
+    [Theory]
+    [ClassData(typeof(NegationDataOperator))]
+    public void ComplementSatisfiesFundamentalProperty(INegation negation)
+    {
+        Assert.StrictEqual(negation.Complement(0), FuzzyNumber.Max);
+        Assert.StrictEqual(negation.Complement(1), FuzzyNumber.Min);
+    }
+
+    [Theory]
+    [ClassData(typeof(IntersectionDataOperator))]
+    public void AssociativityHoldsForIntersection(INorm norm, IEnumerable<(double A, double B, double C)> values)
+    {
+        AssociativityHoldsForIntersectionHelper(norm, values);
+    }
+
+    [Theory]
+    [ClassData(typeof(UnionDataOperator))]
+    public void AssociativityHoldsForUnion(IConorm conorm, IEnumerable<(double A, double B, double C)> values)
+    {
+        AssociativityHoldsForUnionHelper(conorm, values);
+    }
+
+    private static void AssociativityHoldsForIntersectionHelper(INorm norm, IEnumerable<(double A, double B, double C)> values)
+    {
+        foreach (var value in values)
+        {
+            var (x, y, z) = (FuzzyNumber.Of(value.A), FuzzyNumber.Of(value.B), FuzzyNumber.Of(value.C));
+            Assert.Equal(norm.Intersection(x, norm.Intersection(y, z)), norm.Intersection(norm.Intersection(x, y), z));
+        }
+    }
+
+    private static void AssociativityHoldsForUnionHelper(IConorm conorm, IEnumerable<(double A, double B, double C)> values)
+    {
+        foreach (var value in values)
+        {
+            var (x, y, z) = (FuzzyNumber.Of(value.A), FuzzyNumber.Of(value.B), FuzzyNumber.Of(value.C));
+            Assert.Equal(conorm.Union(x, conorm.Union(y, z)), conorm.Union(conorm.Union(x, y), z));
+        }
+    }
+}
+
+file class NegationDataOperator : IEnumerable<object[]>
+{
+    private static readonly IEnumerable<object[]> Negators = IEnum<Negation, NegationType>.Values.Select(e => new object[] {e});
+
+    public IEnumerator<object[]> GetEnumerator() => Negators.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+file class IntersectionDataOperator : IEnumerable<object[]>
+{
+    private static readonly Random Random = new();
+
+    private static readonly IEnumerable<object[]> Intersectors =
+        IEnum<Norm, NormType>.Values.Select(e => new object[] {e, Enumerable.Range(0, 100).Select(_ => (A: Random.NextDouble(), B: Random.NextDouble(), C: Random.NextDouble()))});
+
+    public IEnumerator<object[]> GetEnumerator() => Intersectors.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+file class UnionDataOperator : IEnumerable<object[]>
+{
+    private static readonly Random Random = new();
+
+    private static readonly IEnumerable<object[]> Unitors =
+        IEnum<Conorm, ConormType>.Values.Select(e => new object[] {e, Enumerable.Range(0, 100).Select(_ => (A: Random.NextDouble(), B: Random.NextDouble(), C: Random.NextDouble()))});
+
+    public IEnumerator<object[]> GetEnumerator() => Unitors.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
